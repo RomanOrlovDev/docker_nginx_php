fb9948bc6098e297ec2bdef4834dc7bdc9d9731d
Roman Orlov <r.orlov@eapteka.ru>
2023-01-22 22:20:52 +0300

    profile introduced

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
 <?php
+function runMe(){
+    $x = 'x';
+    for ($i = 0; $i < 10; $i++){
+        $x = $x . $x;
+    }
+}
+runMe();
 $t = 1;
 // use this function to check xdebug errors and warnings
+if (true){
+    runMe();
+}else{
+    runMe();
+}
 xdebug_info();
 exit;
 phpinfo();
